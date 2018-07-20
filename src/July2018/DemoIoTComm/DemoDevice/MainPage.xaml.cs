aa7fff972b05b6e26889ca22dc8261cfd04d4e6e
Teerachai Laothong <admin@digitalthailand.club>
2018-07-20 11:55:22 +0700

    Added Demo Communication between Service and Device

---
+++ src/July2018/DemoIoTComm/DemoDevice/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoDevice
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        DeviceClient deviceClient;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Unloaded += MainPage_Unloaded;
+
+            InitApp().GetAwaiter();
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            deviceClient?.Dispose();
+        }
+
+        private async Task InitApp()
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString("HostName=comiot.azure-devices.net;DeviceId=demo1;SharedAccessKey=wrYYf/gJa52itCl4cVtLa0fxYugPP+2EKlN6H74xKVM=", TransportType.Mqtt);
+            await deviceClient.SetMethodHandlerAsync("demo", DemoMethodHandler, null);
+            await deviceClient.OpenAsync();
+        }
+
+        private async Task<MethodResponse> DemoMethodHandler(MethodRequest req, object context)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                ReceiveListBox.Items.Add(string.Format("Received message @{0}", DateTime.Now.ToLongTimeString()));
+                var json = Newtonsoft.Json.Linq.JObject.Parse(req.DataAsJson);
+                var message = json.Value<string>("Message");
+                var count = json.Value<int>("Count");
+                for (int i = 0; i < count; i++)
+                {
+                    ReceiveListBox.Items.Add(message);
+                }
+            });
+            return new MethodResponse(200);
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            var data = new
+            {
+                Temperature = Convert.ToDouble(TemperatureTextBox.Text),
+                Humidity = Convert.ToDouble(HumidityTextBox.Text)
+            };
+            var dataJson = JsonConvert.SerializeObject(data);
+            var dataBytes = Encoding.UTF8.GetBytes(dataJson);
+            deviceClient.SendEventAsync(new Message(dataBytes));
+
+            SendListBox.Items.Add(string.Format("Send @{0}", DateTime.Now.ToLongTimeString()));
+            SendListBox.Items.Add(dataJson);
+        }
+    }
+}
---
+++ src/July2018/DemoIoTComm/DemoServiceControl/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoServiceControl
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ServiceClient serviceClient;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Unloaded += MainPage_Unloaded;
+
+            InitApp().GetAwaiter();
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            serviceClient?.Dispose();
+        }
+
+        private async Task InitApp()
+        {
+            // TODO: Replace connection string
+            serviceClient = ServiceClient.CreateFromConnectionString("HostName=comiot.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=NiHQ0aP/8RSoOUEfmQItNU4oAdnG+RUgoXOUt/jlYrM=");
+        }
+
+        private async void InvokeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var payload = new
+            {
+                Message = Convert.ToString(MessageTextBox.Text),
+                Count = Convert.ToDouble(CountTextBox.Text)
+            };
+            var payloadJson = JsonConvert.SerializeObject(payload);
+            var c2dMethod = new CloudToDeviceMethod("demo");
+            c2dMethod.SetPayloadJson(payloadJson);
+
+            LogListBox.Items.Add(string.Format("Invoke method @{0}", DateTime.Now.ToLongTimeString()));
+            try
+            {
+                var result = await serviceClient.InvokeDeviceMethodAsync("demo1", c2dMethod);
+                LogListBox.Items.Add(string.Format("Result Status: {0}", result.Status));
+            }
+            catch (Exception ex)
+            {
+                LogListBox.Items.Add(string.Format("Error: {0}", ex));
+            }
+        }
+    }
+}
